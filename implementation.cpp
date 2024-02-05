39149eddcaae15d95c55aacd16ff43d24f95ddd2
Alex Kruglov <x.kryack.x@gmail.com>
2024-02-05 17:44:59 +0300

    1st commit

--- implementation.cpp
+++ implementation.cpp
@@ -1,5 +1,6 @@
 #include "implementation.hpp"
 #include <riscv_vector.h>
+#include <iostream>
 
 // RGB2GRAY conversion
 void impl_rgb2gray(cv::Mat &src, cv::Mat &dst)
@@ -30,8 +31,38 @@ void impl_boxFilter(cv::Mat &src, cv::Mat &dst, int ddepth, cv::Size ksize)
 // Morphology operation - erosion
 void impl_morphology(cv::Mat &src, cv::Mat &dst)
 {
-    static_cast<void>(src);
-    static_cast<void>(dst);
+    std::cout << "morph start" << std::endl;
+    int idx;
+    bool result = 0;
+    uint8_t* pSrc = reinterpret_cast<uint8_t*>(src.data);
+    uint8_t* pDst = reinterpret_cast<uint8_t*>(dst.data);
+    size_t vl = vsetvl_e8m1(4);
+    for (int i = 1; i < src.rows - 1; i++)
+    {
+        for (int j = 1; j < src.cols - 1; j+=vl)
+        {
+            idx = (i * src.cols + j);
+            vuint8m1_t vidx = vle8_v_u8m1(pSrc + idx, vl);
+            vuint8m1_t vidx1 = vle8_v_u8m1(pSrc + idx - src.cols - 1, vl + 2);
+            vuint8m1_t vidx2 = vle8_v_u8m1(pSrc + idx - 1, vl + 2);
+            vuint8m1_t vidx3 = vle8_v_u8m1(pSrc + idx + src.cols - 1, vl + 2);
+            vuint8m1_t vres = vand_vv_u8m1(vidx1, vidx2, vl + 2);
+            vres = vand_vv_u8m1(vres, vidx3, vl + 2);
+            uint8_t* pTmp = new uint8_t*[vl + 2];
+            vse8_v_u8m1(pTmp, vres, vl + 2);
+            for (uint8_t t : pTmp){
+                std::cout << t << ' ';
+            }
+            //vidx1 = vcopy_v_u8m1(vres);
+            // vidx1 = vsll_vx_u8m1(vres, 0, vl);
+            // vidx2 = vsll_vx_u8m1(vres, 1, vl);
+            // vidx3 = vsll_vx_u8m1(vres, 2, vl);
+            // vres = vand_vv_u8m1(vidx1, vidx2, vl);
+            // vres = vand_vv_u8m1(vres, vidx3, vl);
+            //vse8_v_u8m1(pSrc + idx, vres, vl); 
+        }
+    }
+    std::cout << "morph" << std::endl;
 }
 
 // Upscaling 2x
