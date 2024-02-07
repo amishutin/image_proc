cc7647cbdc6c5e502970555ad34bbbfaebd60d44
Lev Neverov <lev.s.neverov@yandex.ru>
2024-02-07 17:03:55 +0300

    Added threshold optimization implementation

---
+++ _build/CMakeFiles/functional_main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/functional_main.dir/functional_main.cpp.o"
+  "CMakeFiles/functional_main.dir/functional_main.cpp.o.d"
+  "CMakeFiles/functional_main.dir/implementation.cpp.o"
+  "CMakeFiles/functional_main.dir/implementation.cpp.o.d"
+  "CMakeFiles/functional_main.dir/ref_implementation.cpp.o"
+  "CMakeFiles/functional_main.dir/ref_implementation.cpp.o.d"
+  "functional_main"
+  "functional_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/functional_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _build/CMakeFiles/perf_main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/perf_main.dir/implementation.cpp.o"
+  "CMakeFiles/perf_main.dir/implementation.cpp.o.d"
+  "CMakeFiles/perf_main.dir/perf_main.cpp.o"
+  "CMakeFiles/perf_main.dir/perf_main.cpp.o.d"
+  "CMakeFiles/perf_main.dir/ref_implementation.cpp.o"
+  "CMakeFiles/perf_main.dir/ref_implementation.cpp.o.d"
+  "perf_main"
+  "perf_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/perf_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
